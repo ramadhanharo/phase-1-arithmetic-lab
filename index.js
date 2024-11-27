545e8411d628ab4a0e1bb5af6554a1f3a1c370ba
ramadhanharo <ramadhan.haro@moringaschool.com>
2024-11-27 12:54:43 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random());
+const num3 = 12;
+const num4 = 8;
+const mod = num3%num4;
+const max = Math.max(20,1,5,8,9);
+
+
